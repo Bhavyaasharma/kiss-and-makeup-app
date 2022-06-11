47f2cba6e5a601862e29714341499b2dabf7c738
Bhavyaasharma <81627599+Bhavyaasharma@users.noreply.github.com>
2022-06-11 12:30:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
 song1 = "";
 song2 = "";
 
 function preload(){
-song1 = loadSound (" music.mp3")
-song2 = loadSound ("Dua Lipa - Kiss and Make Up (feat. BLACKPINK) [129 kbps].mp3")
+song1 = loadSound (" music1.mp3")
+song2 = loadSound ("music2.mp3")
 
 }
 
@@ -13,13 +19,38 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
 }
+
+
+    function modelLoaded(){
+        console.log ('poseNet is initialized');
+    }
 
 function draw(){
     image (video,0,0,600,500);
 }
 
 function songName(){
-    song.play();
+    song1.play();
     song2.play();
 }
+
+function gotPoses(results){
+    if(results.length > 0 )
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY );
+    
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    
+    }
+    
+    }
